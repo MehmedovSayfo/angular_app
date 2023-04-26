a4703a53a9616184077df0f8a5dfe50cb615332e
MehmedovSayfo <sayfo.mehmedov@gmail.com>
2023-04-26 14:18:27 +0300

    Adding src, name and author to our books using interpolation

--- app/src/app/books/books.component.ts
+++ app/src/app/books/books.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
-export class BooksComponent {
+export class BooksComponent implements OnInit{
+  src: string = 'https://m.media-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg';
+  name: string = 'Clean Code';
+  author: string = 'Robert C Martin'
 
+  src2: string = 'https://m.media-amazon.com/images/I/51A8l+FxFNL.jpg';
+  name2: string = 'Pragmatic programmer';
+  author2: string = 'David Thomas'
+
+  ngOnInit(): void {
+  }
 }
